d7ed8cd5f49f2101c18c4b064cfb984790710062
Mohsen Saberi <m.saberi1999@gmail.com>
2022-09-16 21:27:31 +0200

    FinishDevelopmentCatalogMicroserviceAplication

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+	public class CatalogContext : ICatalogContext
+	{
+		public CatalogContext(IConfiguration configuration)
+		{
+			var client = new MongoClient(configuration.GetSection("DatabaseSettings:ConnectionString").ToString());
+			var database = client.GetDatabase(configuration.GetSection("DatabaseSettings:DatabaseName").ToString());
+
+			Products = database.GetCollection<Product>(configuration.GetSection("DatabaseSettings:CollectionName").ToString());
+			//CatalogContextSeed.SeedData(Product);
+		}
+
+		public IMongoCollection<Product> Products { get; }
+	}
+}
