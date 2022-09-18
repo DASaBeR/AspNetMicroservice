ecf1bdd55f9524ccb78e2a0c1223d4691f061a38
Mohsen Saberi <m.saberi1999@gmail.com>
2022-09-18 13:51:30 +0200

    Basket.API microservices developed(repository and controller)

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,3 +1,4 @@
+using Basket.API.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -35,6 +36,13 @@ namespace Basket.API
 			{
 				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
 			});
+
+			#region " IoC "
+
+			services.AddScoped<IBasketRepository, BasketRepository>();
+
+			#endregion
+
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
