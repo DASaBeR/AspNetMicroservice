13e52b66e51f3c5fcd26d51841370ef3d8795d67
Mohsen Saberi <m.saberi1999@gmail.com>
2022-10-13 19:31:42 +0200

    Add UpdateOrder and DeleteOrder also Add Exceptions Classes

--- src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -20,7 +20,7 @@ namespace Ordering.Application.Contracts.Persistence
 																		Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
 																		List<Expression<Func<T, object>>> includes = null,
 																		bool disableTracking = true);
-		Task<T> GetByIdAsync(int id);
+		Task<T> GetByIdAsync(Guid id);
 		Task<T> AddAsync(T entity);
 		Task<T> UpdateAsync(T entity);
 		Task<T> DeleteAsync(T entity);
